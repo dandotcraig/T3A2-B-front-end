813073612e35c73ab60ae9705d31f7b38dd7df5e
dandotcraig <145010207+dandotcraig@users.noreply.github.com>
2024-08-03 22:28:41 +1000

    issues with invoice...

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,20 @@ module.exports = {
       center: true,
       padding: "2rem",
       screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+  
+        'sm': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
       },
     },
     extend: {
